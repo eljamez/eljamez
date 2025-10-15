30ffe67a80292a2676e7e1b06f50d2204e0b49d7
James Hall <eljamez@gmail.com>
2025-10-15 15:54:33 -0700

    new site

---
+++ src/components/home/HomePage4.tsx
@@ -0,0 +1,105 @@
+import { Hash, Music } from "lucide-react";
+import { BsSubstack } from "react-icons/bs";
+import { FaCodepen, FaGithub, FaLinkedin, FaSoundcloud } from "react-icons/fa";
+
+const links = [
+  //   {
+  //     href: "https://bleepsandbloops.com",
+  //     text: "Bleeps and Bloops",
+  //     icon: <Code />,
+  //     description: "Apps and Tools and Things I Make",
+  //   },
+  {
+    href: "https://whatsoever.biz",
+    text: "Whatsoever",
+    icon: <Hash />,
+    description: "An App for Namers",
+  },
+  {
+    href: "https://www.enslymogul.com/",
+    text: "Ensly Mogul",
+    icon: <Music />,
+    description: "A music project",
+  },
+  {
+    href: "https://linkedin.com/in/eljamez",
+    text: "LinkedIn",
+    icon: <FaLinkedin />,
+    description: "My LinkedIn profile",
+  },
+];
+
+const socialLinks = [
+  {
+    href: "https://github.com/eljamez",
+    text: "GitHub",
+    icon: <FaGithub />,
+  },
+  {
+    href: "https://www.linkedin.com/in/eljamez/",
+    text: "LinkedIn",
+    icon: <FaLinkedin />,
+  },
+  {
+    href: "https://substack.com/@jamesaugustushall",
+    text: "Substack",
+    icon: <BsSubstack />,
+  },
+  {
+    href: "https://codepen.io/eljamez",
+    text: "CodePen",
+    icon: <FaCodepen />,
+  },
+  {
+    href: "https://soundcloud.com/eljamez",
+    text: "SoundCloud",
+    icon: <FaSoundcloud />,
+  },
+];
+
+export default function HomePage4() {
+  return (
+    <>
+      <div className="w-full h-auto sm:h-screen bg-slate-900 text-slate-100 flex flex-col p-4 sm:p-8 gap-4">
+        <h1 className="text-[3rem] sm:text-[5rem] md:text-[6rem] font-bold">
+          Hello <span className="text-green-500">I&apos;m James</span>
+        </h1>
+        <h2 className="text-[1rem] sm:text-[2rem] md:text-[3rem] mb-8">
+          I&apos;m a Senior Software Engineer based in Ojai, CA. I enjoy writing
+          code, creating music, and making apps that people like to use.
+        </h2>
+        <ul className="flex flex-col gap-4 text-xl sm:text-4xl sm:p-8 p-4 bg-slate-800 rounded-lg">
+          {links.map((link) => (
+            <li key={link.href}>
+              <a
+                href={link.href}
+                target="_blank"
+                className="hover:text-green-500 group transition-all flex gap-6 items-center"
+              >
+                <span className="group-hover:rotate-180 transition-transform">
+                  {link.icon}
+                </span>
+                {link.text}
+                <span className="text-slate-400 text-sm sm:text-3xl">
+                  {link.description}
+                </span>
+              </a>
+            </li>
+          ))}
+        </ul>
+
+        <div className="flex gap-8 text-lg sm:text-xl flex-1 md:items-end items-center">
+          {socialLinks.map((link) => (
+            <a
+              href={link.href}
+              className="hover:text-green-500 transition-all flex gap-2 items-center"
+            >
+              {link.icon}
+              <span className="hidden sm:inline">{link.text}</span>
+            </a>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
